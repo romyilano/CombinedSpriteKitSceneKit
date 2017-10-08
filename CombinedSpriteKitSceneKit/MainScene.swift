15fc1f9598c570d870c6ecd2746f19e6f5c7b3f6
Romy Ilano <info@romyilano.com>
2017-10-08 11:59:39 -0700

    reverted the init code

--- CombinedSpriteKitSceneKit/MainScene.swift
+++ CombinedSpriteKitSceneKit/MainScene.swift
@@ -18,21 +18,40 @@ class MainScene: SCNScene {
     
     override init() {
         super.init()
-        
-        let cube = SCNBox.init(width: 3, height: 3, length: 3, chamferRadius: 0)
-        
-        let materialScene = SKScene(size: CGSize(width: 100, height: 100))
-        let backgroundNode = SKSpriteNode(color: .blue, size: materialScene.size.height/2.0)
-        backgroundNode.position = CGPoint(x: materialScene.size.width/2.0, y: materialScene.size.height/2.0)
-        materialScene.addChild(backgroundNode)
-        
-        let blueAction = SKAction.colorize(with: .blue, colorBlendFactor: 1, duration: 1)
-        let redAction = SKAction.colorize(with: .red, colorBlendFactor: 1, duration: 1)
-        let greenAction = SKAction.colorize(with: .green, colorBlendFactor: 1, duration: 1)
-        backgroundNode.runAction(SKAction.repeatActionForever(SKAction.sequence[blueAction, redAction, greenAction]))
-        
+        let cube = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0)
         let cubeMaterial = SCNMaterial()
-        cubeMaterial.diffuse.contents =
+        cubeMaterial.diffuse.contents = UIColor.blue
+        cube.materials = [cubeMaterial]
+        self.cubeNode = SCNNode(geometry: cube)
+        self.cubeNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.01, z: 0, duration: 1.0/60.0)))
+        
+        let camera = SCNCamera()
+        camera.xFov = 60
+        camera.yFov = 60
+        
+        let ambientLight = SCNLight()
+        ambientLight.type = SCNLight.LightType.ambient
+        ambientLight.color = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+        
+        let cameraConstraint = SCNLookAtConstraint(target: self.cubeNode)
+        cameraConstraint.isGimbalLockEnabled = true
+        
+        self.cameraNode = SCNNode()
+        self.cameraNode.camera = camera
+        self.cameraNode.constraints = [cameraConstraint]
+        self.cameraNode.light = ambientLight
+        self.cameraNode.position = SCNVector3(x: 5, y: 5, z: 5)
+        
+        let omniLight = SCNLight()
+        omniLight.type = SCNLight.LightType.omni
+        
+        self.lightNode = SCNNode()
+        self.lightNode.light = omniLight
+        self.lightNode.position = SCNVector3(x: -3, y: 5, z: 3)
+        
+        self.rootNode.addChildNode(self.cubeNode)
+        self.rootNode.addChildNode(self.cameraNode)
+        self.rootNode.addChildNode(self.lightNode)
 
     }
 
