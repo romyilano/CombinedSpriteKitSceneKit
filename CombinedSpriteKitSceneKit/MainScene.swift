df456f2c2327215f9ae2e98897768ef02220654d
Romy Ilano <info@romyilano.com>
2017-10-07 23:00:55 -0700

    added crate kitty graphic

--- CombinedSpriteKitSceneKit/MainScene.swift
+++ CombinedSpriteKitSceneKit/MainScene.swift
@@ -19,40 +19,7 @@ class MainScene: SCNScene {
     override init() {
         super.init()
         
-        let cube = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0)
-        let cubeMaterial = SCNMaterial()
-        cubeMaterial.diffuse.contents = UIColor.blue
-        cube.materials = [cubeMaterial]
-        self.cubeNode = SCNNode(geometry: cube)
-        self.cubeNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.01, z: 0, duration: 1.0/60.0)))
         
-        let camera = SCNCamera()
-        camera.xFov = 60
-        camera.yFov = 60
-        
-        let ambientLight = SCNLight()
-        ambientLight.type = SCNLight.LightType.ambient
-        ambientLight.color = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
-        
-        let cameraConstraint = SCNLookAtConstraint(target: self.cubeNode)
-        cameraConstraint.isGimbalLockEnabled = true
-        
-        self.cameraNode = SCNNode()
-        self.cameraNode.camera = camera
-        self.cameraNode.constraints = [cameraConstraint]
-        self.cameraNode.light = ambientLight
-        self.cameraNode.position = SCNVector3(x: 5, y: 5, z: 5)
-        
-        let omniLight = SCNLight()
-        omniLight.type = SCNLight.LightType.omni
-        
-        self.lightNode = SCNNode()
-        self.lightNode.light = omniLight
-        self.lightNode.position = SCNVector3(x: -3, y: 5, z: 3)
-        
-        self.rootNode.addChildNode(self.cubeNode)
-        self.rootNode.addChildNode(self.cameraNode)
-        self.rootNode.addChildNode(self.lightNode)
     }
 
     required init(coder aDecoder: NSCoder) {
